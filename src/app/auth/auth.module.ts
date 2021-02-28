807325af71ff39f47c246eae4de92b134fce8869
prasad <prasadzore31@gmail.com>
2021-02-28 18:00:02 +0530

    Meet module created

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { EditprofileComponent } from './editprofile/editprofile.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { CreatemeetingComponent } from './createmeeting/createmeeting.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, SignupComponent, EditprofileComponent],
+  declarations: [LoginComponent, SignupComponent, EditprofileComponent, CreatemeetingComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
