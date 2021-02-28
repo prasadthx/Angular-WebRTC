e9f94c62e279cc9d64d2ae206dd6184125fada66
prasad <prasadzore31@gmail.com>
2021-02-28 17:13:16 +0530

    Vanta

--- src/app/getstarted/getstarted.component.ts
+++ src/app/getstarted/getstarted.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var VANTA;
 
 @Component({
   selector: 'app-getstarted',
@@ -10,6 +11,19 @@ export class GetstartedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    VANTA.GLOBE({
+      el: "#my-background",
+      mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x343ab1,
+  size: 0.80,
+  backgroundColor: 0xd0a14
+    })
   }
 
 }
