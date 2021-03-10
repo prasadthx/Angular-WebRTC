09c192b446e24801327ba046d401fc8d29a9ba7b
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-03-10 21:57:22 +0530

    Styling for first

--- src/app/auth/account/layout/layout.component.ts
+++ src/app/auth/account/layout/layout.component.ts
@@ -4,7 +4,9 @@ import {AccountService} from "../../services/account.service";
 declare var VANTA;
 
 
-@Component({ templateUrl: 'layout.component.html' })
+@Component({ templateUrl: 'layout.component.html',
+  styleUrls: ['./layout.component.css']}
+)
 export class LayoutComponent implements OnInit{
     constructor(
         private router: Router,
