5b42588bf8062297a9df294553012e2aa12c1221
prasad <prasadzore31@gmail.com>
2021-03-11 01:22:30 +0530

    Login work

--- src/app/auth/home/home.component.ts
+++ src/app/auth/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 import { AccountService } from '../services/account.service';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: 'home.component.html',
+styleUrls:['home.component.css'] })
 export class HomeComponent {
     account = this.accountService.accountValue;
 
